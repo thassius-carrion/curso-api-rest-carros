39283d9dacea5009eaf79360ba52a2a4b234b206
THASSIUS <thassius.carrion@hotmail.com>
2021-07-01 16:15:51 -0300

    adicionando Lombok no projeto

--- src/main/java/com/tlove/carros/domain/Carro.java
+++ src/main/java/com/tlove/carros/domain/Carro.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.*;
+
 @Entity
+@Data
 public class Carro {
 	
 	@Id
@@ -16,39 +19,4 @@ public class Carro {
 	
 	private String tipo;
 	
-	public Carro(Long id, String nome, String tipo) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.tipo = tipo;
-	}
-	
-	public Carro() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	
-
 }
